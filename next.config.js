a7d831c4cb5f86f1542c326c6a9cd4476ecbf798
Tim Dorr <git@timdorr.com>
2022-09-27 13:59:54 -0400

    Add a redirect for demo day

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/atl(anta)?(-cox)?-demo-day(-2022)?',
+        destination:
+          'https://www.eventbrite.com/e/techstars-atlanta-powered-by-cox-enterprises-demo-day-2022-tickets-394449387737',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
