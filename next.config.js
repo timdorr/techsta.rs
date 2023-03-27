0c18bb5d72f110604e728d858f4fc2490917ad27
Tim Dorr <git@timdorr.com>
2023-03-27 13:57:52 -0400

    Add slack redirects

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   async redirects() {
     return [
       {
         source: '/atl(anta)?(-cox)?-demo-day(-2022)?',
         destination:
           'https://www.eventbrite.com/e/techstars-atlanta-powered-by-cox-enterprises-demo-day-2022-tickets-394449387737',
-        permanent: true,
+        permanent: true
       },
+      {
+        source: '/atl(anta)?(-cox)?-slack',
+        destination:
+          'https://join.slack.com/t/techstarscox/shared_invite/zt-1e7tjypjl-DHXE0GJhcVX~Y21g_PMzoQ',
+        permanent: true
+      },
+      {
+        source: '/impact(-cox)?-slack',
+        destination:
+          'https://join.slack.com/t/techstarscox/shared_invite/zt-1e7tjypjl-DHXE0GJhcVX~Y21g_PMzoQ',
+        permanent: true
+      }
     ]
-  },
+  }
 }
 
 module.exports = nextConfig
