70dc3d7c4d6ac39b9d9d93069634cab32a8013a0
Tim Dorr <git@timdorr.com>
2024-02-16 16:26:39 -0500

    Add some metadata to Atlanta

--- pages/atlanta/index.tsx
+++ pages/atlanta/index.tsx
@@ -1,3 +1,14 @@
+import Head from 'next/head'
+
 export default function Atlanta() {
-  return <div>Coming soon!</div>
+  return (
+    <>
+      <Head>
+        <title>Techstars Atlanta</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>Coming soon!</div>
+    </>
+  )
 }
